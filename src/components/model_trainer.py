9633a442523d83061415be3f242eedbba48c1073
rajat3154 <rajaturanvir31@gmail.com>
2025-02-27 21:12:41 +0530

    Hyperparamter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,13 +1,13 @@
-import os 
-import sys 
+import os
+import sys
 from dataclasses import dataclass
 from catboost import CatBoostRegressor
-from sklearn.ensemble import(
-      RandomForestRegressor,
-      GradientBoostingRegressor,
-      AdaBoostRegressor   
+from sklearn.ensemble import (
+    RandomForestRegressor,
+    GradientBoostingRegressor,
+    AdaBoostRegressor
 )
-from sklearn.linear_model import LinearRegression,Ridge,Lasso 
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
 from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
@@ -15,52 +15,97 @@ from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging
-from src.utils import save_object,evaluate_models
+from src.utils import save_object, evaluate_models  # Make sure evaluate_models is updated if needed.
 
-@dataclass 
+@dataclass
 class ModelTrainerConfig:
-      trained_model_file_path=os.path.join('artifacts','model.pkl')
+    trained_model_file_path = os.path.join('artifacts', 'model.pkl')
+
 class ModelTrainer:
-      def __init__(self):
-            self.model_trainer_config=ModelTrainerConfig()
-      def initiate_model_trainer(self,train_array,test_array):
-            try:
-                  logging.info('Splitting training and test input data')
-                  X_train,y_train,X_test,y_test=(
-                        train_array[:,:-1],
-                        train_array[:,-1],
-                        test_array[:,:-1],
-                        test_array[:,-1]
-                  )
-                  models={
-                  'Linear Regression':LinearRegression(),
-                  'Lasso':Lasso(),
-                  'Ridge':Ridge(),
-                  'K-Neighbors Regressor':KNeighborsRegressor(),
-                  'Decision Tree':DecisionTreeRegressor(),
-                  'Random Forest Regressor':RandomForestRegressor(),
-                  'XGBREgressor':XGBRegressor(),
-                  'CatBoosting Regressor':CatBoostRegressor(verbose=False),
-                  'AdaBoost Regressor':AdaBoostRegressor()
-                  }
-                  model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
-                  
-                  best_model_score=max(sorted(model_report.values()))
-                  best_model_name=list(model_report.keys())[
-                        list(model_report.values()).index(best_model_score)
-                  ]
-                  best_model=models[best_model_name]
-                  if best_model_score<0.6:
-                        raise CustomException('No best model found')
-                  logging.info(f'Best model found on both both training and testing dataset')
-                  
-                  save_object(
-                        file_path=self.model_trainer_config.trained_model_file_path,
-                        obj=best_model
-                  )
-                  predicted=best_model.predict(X_test)
-                  r2_square=r2_score(y_test,predicted)
-                  return r2_square
-            except Exception as e:
-                  raise CustomException(e,sys) 
-                  
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_array, test_array):
+        try:
+            logging.info('Splitting training and test input data')
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1]
+            )
+
+            models = {
+                'Linear Regression': LinearRegression(),
+                'Lasso': Lasso(),
+                'Ridge': Ridge(),
+                'K-Neighbors Regressor': KNeighborsRegressor(),
+                'Decision Tree': DecisionTreeRegressor(),
+                'Random Forest Regressor': RandomForestRegressor(),
+                'Gradient Boosting Regressor': GradientBoostingRegressor(),
+                'XGBRegressor': XGBRegressor(),
+                'CatBoosting Regressor': CatBoostRegressor(verbose=False),
+                'AdaBoost Regressor': AdaBoostRegressor()
+            }
+
+            # This params dict is unused unless you modify evaluate_models to support it.
+            params = {
+                'Decision Tree': {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson']
+                },
+                'Random Forest Regressor': {
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                'Gradient Boosting Regressor': {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                'Linear Regression': {},
+                 'Lasso': {},               # Add this
+                   'Ridge': {},
+                'K-Neighbors Regressor': {
+                    'n_neighbors': [5, 7, 9, 11],
+                },
+                'XGBRegressor': {
+                    'learning_rate': [.1, .01, 0.5, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                'CatBoosting Regressor': {
+                    'depth': [6, 8, 10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100],
+                },
+                'AdaBoost Regressor': {
+                    'learning_rate': [.1, .01, 0.5, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256],
+                }
+            }
+
+            logging.info('Evaluating models')
+
+            # Call evaluate_models (update this if you add hyperparameter tuning)
+            model_report, best_model = evaluate_models(X_train, y_train, X_test, y_test, models, params)
+
+
+            best_model_score = max(model_report.values())
+
+            best_model_name = [key for key, value in model_report.items() if value == best_model_score][0]
+            best_model = models[best_model_name]
+
+            if best_model_score < 0.6:
+                raise CustomException('No best model found')
+
+            logging.info(f'Best model found: {best_model_name} with R2 score: {best_model_score}')
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted = best_model.predict(X_test)
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
+
+        except Exception as e:
+            raise CustomException(e, sys)
--- src/utils.py
+++ src/utils.py
@@ -1,33 +1,54 @@
-import os 
-import sys 
+import os
+import sys
 import dill
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import r2_score
 from src.exception import CustomException
 
-def save_object(file_path,obj):
-      try:
-            dir_path=os.path.dirname(file_path)
-            os.makedirs(dir_path,exist_ok=True)
-            with open(file_path,'wb') as file_obj:
-                  dill.dump(obj,file_obj)
-                  
-      except Exception as e:
-            raise CustomException(e,sys) 
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            dill.dump(obj, file_obj)
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
-      try:
-            report={}
-            for i in range(len(list(models))):
-                  model=list(models.values())[i]
-                  model.fit(X_train,y_train)
-                  y_train_pred=model.predict(X_train)
-                  y_test_pred=model.predict(X_test)
-                  train_model_score=r2_score(y_train,y_train_pred)
-                  test_model_score=r2_score(y_test,y_test_pred)
-                  report[list(models.keys())[i]]=test_model_score
-            return report 
-      except Exception as e:
-            raise CustomException(e,sys)
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
+    try:
+        report = {}
+        best_model = None
+        best_score = -np.inf
+
+        for model_name, model in models.items():
+            try:
+                param_grid = params.get(model_name, {})
+
+                if param_grid:  
+                    gs = GridSearchCV(model, param_grid, cv=3, scoring='r2', n_jobs=-1)
+                    gs.fit(X_train, y_train)
+                    best_model_instance = gs.best_estimator_
+                else:  
+                    best_model_instance = model
+                    best_model_instance.fit(X_train, y_train)
+
+                y_test_pred = best_model_instance.predict(X_test)
+                test_score = r2_score(y_test, y_test_pred)
+
+                report[model_name] = test_score
+
+                if test_score > best_score:
+                    best_score = test_score
+                    best_model = best_model_instance
+
+            except Exception as model_exception:
+                print(f"Skipping {model_name} due to error: {model_exception}")
+        return report, best_model
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
